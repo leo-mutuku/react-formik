02dba0399aeaa6de8ea5356226fa3d6733c94945
leo-mutuku <le.mutuku@gmail.com>
2023-09-23 20:51:10 +0300

    added submit and values

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,41 @@
-import { useState } from 'react'
-import {useFormik} from 'formik'
 import './App.css'
 import { useFormik } from 'formik'
-
 function App() {
-  const [count, setCount] = useState(0)
-  const formik = useFormik({})
+  const formik = useFormik({
+    initialValues:{
+      name: '',
+      email:'',
+      channel:''
+    },
+    onSubmit:values =>{
+      console.log('Form data', values)
+    }
+  })
 
+  // console.log(formik.values)
   return (
     <>
      <div>
-      <form>
+      <form onSubmit={formik.handleSubmit}>
+        <div>
         <label htmlFor='name'>Name</label>
-        <input type='text' id="name" name="name" />
+        <input 
+          type='text' 
+          id="name" 
+          name="name" 
+          onChange={formik.handleChange} 
+          value={formik.values.name}
+          />
+        </div>
 
         <label htmlFor='email'>Email</label>
-        <input type='email' id="name" name="name" />
-        
+        <input type='email' id="email" name="email" onChange={formik.handleChange} value = {formik.values.email} />
+        <div>
         <label htmlFor='name'>Channel</label>
-        <input type='text' id="name" name="name" />
-
+        <input type='text' id="channel" name="channel" onChange={formik.handleChange} value={formik.values.channel}/>
+        </div>
         <button type='submit'>Submit</button>
+
       </form>
      </div>
     </>
